760b71009b94a2fe2336f7e7abff138013c3c28a
ltgbao04 <giabaoletran96@gmail.com>
2025-06-28 15:49:31 +0700

    Decrease number of tools used

--- roles/collector.py
+++ roles/collector.py
@@ -13,22 +13,28 @@ class Collector(Role):
         "Perform a full scan of the target to identify all open ports and services."
     )
 
+    # tools: str = (
+    #     "Nmap, "
+    #     "Curl, "
+    #     "Wget, "
+    #     "Tcpdump, "
+    #     "Whois, "
+    #     "Dmitry, "
+    #     "Dnsenum, "
+    #     "Netdiscover, "
+    #     "Amap, "
+    #     "Enum4linux, "
+    #     "Smbclient, "
+    #     "Amass, "
+    #     "SSLscan, "
+    #     "SpiderFoot, "
+    #     "Fierce."
+    # )
+    
     tools: str = (
         "Nmap, "
         "Curl, "
         "Wget, "
-        "Tcpdump, "
-        "Whois, "
-        "Dmitry, "
-        "Dnsenum, "
-        "Netdiscover, "
-        "Amap, "
-        "Enum4linux, "
-        "Smbclient, "
-        "Amass, "
-        "SSLscan, "
-        "SpiderFoot, "
-        "Fierce."
     )
 
     prompt: ClassVar[CollectorPrompt] = CollectorPrompt
